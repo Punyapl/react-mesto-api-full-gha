ba7969c82936dcc54b6f222a49ac81857094a0bd
punyapl <vasabi.light2005@gmail.com>
2023-10-06 02:08:05 +0700

    fix: фикс url базы данных

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,4 +1,5 @@
-const { NODE_ENV, JWT_SECRET, MONGODB_URL } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
+const { MONGODB_URL = 'mongodb://130.193.36.108:27017/mestodb' } = process.env;
 
 module.exports = {
   NODE_ENV,
